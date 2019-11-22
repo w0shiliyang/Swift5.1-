bcfbfe13eccfe3db2168b59f726fa234cc1b17be
liy <liy@weipaitang.com>
2019-11-22 09:55:29 +0800

    添加下标、继承知识点

--- Swift5.1基础.playground/Pages/下标.xcplaygroundpage/Contents.swift
+++ Swift5.1基础.playground/Pages/下标.xcplaygroundpage/Contents.swift
@@ -1,3 +1,159 @@
 import Foundation
 
+//:下标
+/// 使用subscript可以给任意类型（枚举、结构体、类）增加下标功能，有些地方也翻译为：下标脚本
+/// subscript的语法类似于实例方法、计算属性、本质就是方法（函数）
 
+class Point {
+    var x = 0.0, y = 0.0
+    subscript(index: Int) -> Double {
+        set {
+            if index == 0 {
+                x = newValue
+            } else if index == 1 {
+                y = newValue
+            }
+        }
+        
+        get {
+            if index == 0 {
+                return x
+            } else if index == 1 {
+                return y
+            }
+            return 0
+        }
+    }
+}
+
+var p = Point()
+p[0] = 11.1
+p[1] = 22.2
+print(p.x) // 11.1
+print(p.y) // 22.2
+print(p[0]) // 11.1
+print(p[1]) // 22.2
+
+/// subscript中定义的返回值类型决定了
+/// get方法的返回值类型
+/// set方法中newValue的类型
+/// subscript可以接受多个参数，并且任意类型
+
+//: 下标的细节
+/// subscript可以没有set方法，但必须要有get方法
+class Point1 {
+    var x = 0.0, y = 0.0
+    subscript(index: Int) -> Double {
+        get {
+            if index == 0 {
+                return x
+            } else if index == 1 {
+                return y
+            }
+            return 0
+        }
+    }
+}
+
+/// 如果只有get方法，可以省略get
+class Point2 {
+    var x = 0.0, y = 0.0
+    subscript(index: Int) -> Double {
+        if index == 0 {
+            return x
+        } else if index == 1 {
+            return y
+        }
+        return 0
+    }
+}
+
+//: 下标的细节
+/// 可以设置参数标签
+class Point3 {
+    var x = 0.0, y = 0.0
+    subscript(index i: Int) -> Double {
+        get {
+            if i == 0 {
+                return x
+            } else if i == 1 {
+                return y
+            }
+            return 0
+        }
+    }
+}
+
+var p3 = Point3()
+p3.y = 22.2
+print(p3[index: 1]) // 22.2
+
+/// 下标可以是类型方法
+class Sum {
+    static subscript(v1: Int, v2: Int) -> Int {
+        return  v1 + v2
+    }
+}
+print(Sum[10, 20]) // 30
+
+//: 结构体、类作为返回值对比(没啥区别)
+class Point4 {
+    var x = 0, y = 0
+}
+class PointManager {
+    var point = Point()
+    subscript(index: Int) -> Point {
+        get { point }
+    }
+}
+
+var pm = PointManager()
+pm[0].x = 11
+pm[0].y = 22
+// Point(x: 11, y: 22)
+print(pm[0])
+// Point(x: 11, y: 22)
+print(pm.point)
+
+struct Point5 {
+    var x = 0, y = 0
+}
+
+class PointManager2 {
+    var point = Point()
+    subscript(index: Int) -> Point {
+        set { point = newValue }
+        get { point }
+    }
+}
+
+//: 接收多个参数的下标
+class Grid {
+    var data = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ]
+    subscript(row: Int, column: Int) -> Int {
+        set {
+            guard row >= 0 && row < 3 && column >= 0 && column < 3 else {
+                return
+            }
+            data[row][column] = newValue
+        }
+        get {
+            guard row >= 0 && row < 3 && column >= 0 && column < 3 else {
+                return 0
+            }
+            return data[row][column]
+        }
+    }
+}
+
+var grid = Grid()
+grid[0, 1] = 77
+grid[1, 2] = 88
+grid[2, 0] = 99
+print(grid.data)
+
+//: [继承](@next)
---
+++ Swift5.1基础.playground/Pages/继承.xcplaygroundpage/Contents.swift
@@ -0,0 +1,278 @@
+import Foundation
+//: 继承（Inheritance）
+/// 值类型（枚举、结构体）不支持继承，只有类支持继承
+/// 没有父类的类，称为：基类
+/// Swift并没有像OC、Java那样的规定： 任意类都最终都要继承自某个基类
+/// 子类可以重写父类的下标、方法、属性，重写必须加上override关键字
+//:重写实例方法、下标
+class Animal {
+    func speak() {
+        print("Animal speek")
+    }
+    subscript(index: Int) -> Int {
+        return index
+    }
+}
+
+var anim: Animal
+anim = Animal()
+// Animal speak
+anim.speak()
+// 6
+print(anim[6])
+
+class Cat: Animal {
+    override func speak() {
+        super.speak()
+        print("Cat speak")
+    }
+    override subscript(index: Int) -> Int {
+        return super[index] + 1
+    }
+}
+
+anim = Cat()
+// Animal speak
+// Cat speak
+anim.speak()
+// 7
+print(anim[6])
+
+/// 被class修饰的类型方法、下标，允许被子类重写
+/// 被static修饰的类型方法、下标，不允许被子类重写
+
+//: 重写属性
+/// 子类可以将父类的属性（存储、计算）重写为计算属性
+/// 子类不可以将父类属性重写为存储属性
+/// 只能重写var属性，不能重写let属性
+/// 重写时，属性名、类型要一致
+
+/// 子类重写后的属性权限 不能小于 父类属性的权限
+/// 如果父类属性是只读的，那么子类z重写后的属性可以是只读的、也可以是可读写的
+/// 如果父类属性是可读写的，那么子类重写后的属性也可以是可读写的
+//:重写实例属性
+class Circle {
+    var radius: Int = 0
+    var diameter: Int {
+        set {
+            print("Circle setDiameter")
+            radius = newValue / 2
+        }
+        get {
+            print("Circle getDiameter")
+            return radius * 2
+        }
+    }
+}
+
+var circle: Circle
+circle = Circle()
+circle.radius = 6
+// Circle getDiamter
+// 12
+print(circle.diameter)
+// Circle setDiameter
+circle.diameter = 20
+// 10
+print(circle.radius)
+
+class SubCircle : Circle {
+    override var radius: Int {
+        set {
+            print("SubCircle setRadius")
+            super.radius = newValue > 0 ? newValue : 0
+        }
+        get {
+            print("SubCircle getRadius")
+            return super.radius
+        }
+    }
+    override var diameter: Int {
+        set {
+            print("SubCircle setDiameter")
+            super.diameter = newValue > 0 ? newValue : 0
+        }
+        get {
+            print("SubCircle getDiameter")
+            return super.diameter
+        }
+    }
+}
+
+circle = SubCircle()
+
+//SubCircle setRaius
+circle.radius = 6
+
+// SubCircle getDiameter
+// Circle getDiameter
+// SubCircle getRadius
+// 12
+print(circle.diameter)
+
+// SubCircle setDiameter
+// Circle setDiameter
+// SubCircle setRaius
+circle.diameter = 12
+
+// SubCircle getRaius
+// 10
+print(circle.radius)
+
+//: 重写类型属性
+/// 被class修饰的计算类型属性，可以被子类重写
+/// 被static修饰的类型属性（存储、计算），不可以被子类重写
+class Circle1 {
+    static var radius: Int = 0
+    class var diameter: Int {
+        set {
+            print("Circle setDiameter")
+        }
+        get {
+            print("Circle getDiameter")
+            return radius * 2
+        }
+    }
+}
+
+class SubCircle1: Circle1 {
+    override static var diameter: Int {
+        set {
+            print("SubCircle setDiameter")
+            super.diameter = newValue > 0 ? newValue : 0
+        }
+        get {
+            print("SubCircle getDiameter")
+            return super.diameter
+        }
+    }
+}
+Circle1.radius = 6
+// Circle getDiameter
+// 12
+print(Circle1.diameter)
+// Circle setDiameter
+Circle1.diameter = 20
+// 10
+print(Circle1.radius)
+
+SubCircle1.radius = 6
+// SubCircle getDiameter
+// Circle getDiameter
+// 12
+print(SubCircle1.diameter)
+// SubCircle setDiameter
+// Circle setDiameter
+SubCircle1.diameter = 20
+// 10
+print(SubCircle1.radius)
+//: 属性观察器
+///1、 可以在子类中为父类属性（除了只读计算属性、let属性）增加属性观察器
+class TestCircle {
+    var radius: Int = 1
+}
+class TestSubCircle: TestCircle {
+    override var radius: Int {
+        willSet {
+            print("SubCircle willSetRadius", newValue)
+        }
+        didSet {
+            print("SubCircle didSetRadius", oldValue, radius)
+        }
+    }
+}
+var testCircle = TestCircle()
+// SubCircle willSetRadius 10
+// SubCircle didSetRadius 1 10
+testCircle.radius = 10
+
+///2、
+class TestCircle2 {
+    var radius: Int = 1 {
+        willSet {
+            print("Cricle willSetRadius", newValue)
+        }
+        didSet {
+            print("Circle didSetRaridus", oldValue, radius)
+        }
+    }
+}
+class TestSubCircle2: TestCircle2 {
+    override var radius: Int {
+        willSet {
+            print("SubCircle willSetRadius", newValue)
+        }
+        didSet {
+            print("SubCircle didSetRadius", oldValue, radius)
+        }
+    }
+}
+
+var testCircle2 = TestSubCircle2()
+// SubCircle willSetRadius 10
+// Circle willSetRadius 10
+// Circle didSetRadius 1 10
+// SubCircle didSetRadius 1 10
+testCircle2.radius = 10
+
+/// 3、
+class Circle3 {
+    var radius: Int {
+        set {
+            print("Circle setRadius", newValue)
+        }
+        get {
+            print("Circle getRadius")
+            return 20
+        }
+    }
+}
+class SubCircle3: Circle3 {
+    override var radius: Int {
+        willSet {
+            print("SubCircle willSetRadius", newValue)
+        }
+        didSet {
+            print("SubCircle didSetRadius", oldValue, radius)
+        }
+    }
+}
+var circle3 = SubCircle3()
+// Circle getRadius
+// SubCircle willSetRadius 10
+// Circle setRadius 10
+// Circle getRadius
+// SubCircle didSetRadius 20 20
+circle3.radius = 10
+
+//4、
+class Circle4 {
+    class var radius: Int {
+        set {
+            print("Circle setRadius", newValue)
+        }
+        get {
+            print("Circle getRadius")
+            return 20
+        }
+    }
+}
+class SubCircle4: Circle4 {
+    override static var radius: Int {
+        willSet {
+            print("SubCircle willSetRadius", newValue)
+        }
+        didSet {
+            print("SubCircle didSetRadius", oldValue, radius)
+        }
+    }
+}
+// Circle getRadius
+// SubCircle willSetRadius 10
+// Circle setRadius 10
+// Circle getRadius
+// SubCircle didSetRadius 20 20
+SubCircle4.radius = 10
+//: final
+/// 被final修饰的方法、下标、属性，禁止被重写
+/// 被final修饰的类，禁止被继承
