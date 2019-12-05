bdd369826bf73faa0702c3c01603248c309e6177
liy <liy@weipaitang.com>
2019-12-05 10:08:32 +0800

    update

--- Swift5.1基础.playground/Pages/错误处理.xcplaygroundpage/Contents.swift
+++ Swift5.1基础.playground/Pages/错误处理.xcplaygroundpage/Contents.swift
@@ -111,7 +111,7 @@ do {
 //: rethrows
 /// rethrows表明：函数本身不会抛出错误，但调用闭包参数抛出错误，那么它会将错误上抛
 func exec(_ func:(Int, Int) throws -> Int, _ num1: Int, _ num2: Int) rethrows {
-    print(try divide(num1, num2))
+//    print(try divide(num1, num2))
 }
 // Fatal error: Error raised at top level
 try exec(divide, 20, 0)
